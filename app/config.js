296a9ee18d86cc9781bd38dad6519c8af6b224cc
Tom Ridd <twridd@gmail.com>
2020-09-10 17:59:06 +0100

    update service title

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Shielded Support Toolkit - Prototype',
+  serviceName: 'Coronavirus Support Service - Prototype',
 
   // Default port that prototype runs on
   port: '3000',
